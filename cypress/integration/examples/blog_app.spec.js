28ce4c225e3e535f9f1bcf68700d6518b200f17d
Oskar <gustosk@gmail.com>
2020-08-31 10:01:08 +0300

    Tehtävä 5.17
    added cypress and basic test

---
+++ cypress/integration/examples/blog_app.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+describe('Blog app', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      cy.visit('http://localhost:3000')
+    })
+  
+    it('Login from is shown', function() {
+      cy.visit('http://localhost:3000')
+      cy.contains('Login')        
+    })
+  })
